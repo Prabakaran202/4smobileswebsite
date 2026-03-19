db70f1f1d9f11223fad02e04a7c6e41cb572556f
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-03-19 16:33:48 +0530

    Update database.py
    update

--- 4smobiles-backend/database.py
+++ 4smobiles-backend/database.py
@@ -1,1 +1,23 @@
-database.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
